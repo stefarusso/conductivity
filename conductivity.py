a5bf30545e52fd10fd4edfa9033d67df5db25a24
stefarusso <stefarusso@rush.steve95@gmail.com>
2022-11-16 12:58:33 +0100

    added self-diffusion calc

--- conductivity.py
+++ conductivity.py
@@ -1,6 +1,11 @@
 import numpy as np
+import matplotlib
+import matplotlib.pyplot as plt
+from sklearn.linear_model import LinearRegression
+
+
 #file='test/40_out.xyz'
-file='test/200.xyz.xyz'
+file='../test/200.xyz'
 
 #time step in picoseconds
 dt=0.1
@@ -33,7 +38,7 @@ def process_frame(f):
 	#initialize variables
 	q_frame, x_frame, y_frame,z_frame= [],[],[],[]
 	#cicle over molecules
-	for i in range(2000):
+	for i in range(n_mol):
 		charge , coord = process_line(read_line(f))	
 		q_frame.append(charge)
 		x_frame.append(coord[0])
@@ -69,7 +74,7 @@ q=np.array(q)
 #is a tuple of array, one for axis
 cation_idx=np.where(q == 1)
 print(cation_idx[0].shape)
-anion_idx=np.where(q == 1)
+anion_idx=np.where(q == -1)
 
 print("LOADING TRAJECTORY FILE : COMPLETE")
 print("X : ",x.shape)
@@ -83,7 +88,8 @@ dz=z[:,:]-z[0,:]
 
 r2=np.multiply(dx,dx)+np.multiply(dy,dy)+np.multiply(dz,dz)
 #UNIT CHANGES FROM A^2/ps -> m^2/s
-r2=r2*1e-08
+unit_conversion=1e-8
+r2=r2*unit_conversion
 print("|R-R0|^2 : ",r2.shape)
 
 r2_cation=r2[:,cation_idx[0]]
@@ -92,19 +98,60 @@ r2_anion=r2[:,anion_idx[0]]
 print("ANION SUB DATA : ",r2_cation.shape)
 
 t=np.arange(0,r2.shape[0]*dt,dt)
+print("T : ", t.shape )
 
 msd_cation=np.sum(r2_cation,axis=1)/r2_cation.shape[1]
 print("MSD_CATION : ",msd_cation.shape)
 msd_anion=np.sum(r2_anion,axis=1)/r2_anion.shape[1]
 print("MSD_ANION : ",msd_anion.shape)
 
-import matplotlib
-import matplotlib.pyplot as plt
 
-fig, ax = plt.subplots()
-ax.plot(t,msd_cation,linewidth=1.5       ,label=r'msd',color='red',zorder=2)
+
+
+
+fig, ax = plt.subplots(1,2)
+ax[0].plot(t,msd_cation,linewidth=1.5,label=r'msd mean',color='red',zorder=2)
 for mol in r2_cation.T:
-	 ax.plot(t,mol,linewidth=0.5,alpha=0.2,label=r'msd',color='black',zorder=1)
+	 ax[0].plot(t,mol,linewidth=0.5,alpha=0.2,color='black',zorder=1)
+
+#linear regression of the 60% of the msd
+scaling=0.3
+
+idx=int(len(t)*scaling)
+t_subset=t[idx:].reshape((-1,1))
+msd_subset=msd_cation[idx:]
+print("SUB_T : " ,t_subset.shape)
+print("SUB_MSD : ",msd_subset.shape)
+model = LinearRegression().fit(t_subset,msd_subset)
+print(f"D : {model.coef_*6} m^2/s")
+msd_pred = model.predict(t_subset)
+ax[0].plot(t_subset,msd_pred,label='linear regression',linewidth=1,linestyle='dashed',color='orange',zorder=3)
+ax[0].legend()
+ax[0].set_title("Cation MSD")
+ax[0].set_xlabel(r'time / ps')
+ax[0].set_ylabel(r'MSD / m^2/s')
+ax[0].set_ylim([0,max(msd_cation)*3])
+ax[0].set_box_aspect(1)
+
+ax[1].plot(t,msd_anion,linewidth=1.5,label=r'msd mean',color='red',zorder=2)
+for mol in r2_anion.T:
+	 ax[1].plot(t,mol,linewidth=0.5,alpha=0.2,color='black',zorder=1)
+idx=int(len(t)*scaling)
+t_subset=t[idx:].reshape((-1,1))
+msd_subset=msd_anion[idx:]
+print("SUB_T : " ,t_subset.shape)
+print("SUB_MSD : ",msd_subset.shape)
+model = LinearRegression().fit(t_subset,msd_subset)
+print(f"D : {model.coef_*6} m^2/s")
+msd_pred = model.predict(t_subset)
+ax[1].plot(t_subset,msd_pred,label='linear regression',linewidth=1,linestyle='dashed',color='orange',zorder=3)
+ax[1].legend()
+ax[1].set_title("Anion MSD")
+ax[1].set_ylim([0,max(msd_anion)*3])
+ax[1].set_xlabel(r'time / ps')
+ax[1].set_ylabel(r'MSD / m^2/s')
+ax[1].set_box_aspect(1)
+fig.tight_layout()
 plt.show()
 
 
