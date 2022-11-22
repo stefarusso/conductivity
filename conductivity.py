287e151d120d3814f6ff056d79d8f57bf5c6998f
stefarusso <stefarusso@rush.steve95@gmail.com>
2022-11-22 16:56:42 +0100

    overall clean-up

--- conductivity.py
+++ conductivity.py
@@ -47,6 +47,56 @@ def process_frame(f):
 		z_frame.append(coord[2])
 	return q_frame,x_frame,y_frame,z_frame
 
+def regression(msd,t,scaling=0.3):
+	#
+	#Use skitlearn tools for making the linear regression
+	#
+	idx=int(len(t)*scaling)
+	t_pred=t[idx:].reshape((-1,1))
+	msd_subset=msd[idx:]
+	print("SUB_T : " ,t_pred.shape)
+	print("SUB_MSD : ",msd_subset.shape)
+	model_c = LinearRegression().fit(t_pred,msd_subset)
+	print(f"slope CATION : {model_c.coef_} ")
+	print(f"Intercept: {model_c.intercept_} ")
+	print(f"D CATION : {model_c.coef_/6} pm^2/ps")
+	msd_pred = model_c.predict(t_pred)
+	return msd_pred, t_pred
+
+def plotting(t,msd,t_pred,msd_pred):
+	#
+	# Simple plotting of raw MSD and linear regression line
+	#
+	fig, ax = plt.subplots()
+	ax.plot(t,msd,linewidth=1.3,label=r'msd',color='red',zorder=2)
+	ax.plot(t_pred,msd_pred,label='linear regression',linewidth=1,linestyle='dashed',color='blue',zorder=3)
+	ax.legend()
+	ax.set_title("Cation MSD")
+	ax.set_xlabel(r'time / ps')
+	ax.set_ylabel(r'MSD / pm^2')
+	plt.show()
+
+
+
+#-------------------------------------------------
+#DATA
+travis_data=pd.read_csv("../test/lys/lys_c_travis.csv",sep='; ',header=0,engine='python')
+travis_data.columns = ['t', 'msd', 'derivative']
+travis_data_a=pd.read_csv("../test/lys/lys_a_travis.csv",sep='; ',header=0,engine='python')
+travis_data_a.columns = ['t', 'msd', 'derivative']
+vmd_data_c=pd.read_csv("../test/lys/vmd_c.dat",sep=' ',header=None)
+vmd_data_c.columns = ['t', 'msd']
+vmd_data_a=pd.read_csv("../test/lys/vmd_a.dat",sep=' ',header=None)
+vmd_data_a.columns = ['t', 'msd']
+vmd_data_a.t=vmd_data_a.t*1e3
+vmd_data_a.msd=vmd_data_a.msd*1e4
+vmd_data_c.t=vmd_data_c.t*1e3
+vmd_data_c.msd=vmd_data_c.msd*1e4
+#DATA 
+#----------------------------
+
+
+
 
 #TRAJECTORY LOADING---------------------------------
 try:
@@ -66,212 +116,72 @@ except End_of_Loop:
 	print("End of File")
 	pass
 
-#-------------------------------------------------
-#Final coord vectors
-travis_data=pd.read_csv("../test/lys/lys_c_travis.csv",sep='; ',header=0,engine='python')
-travis_data.columns = ['t', 'msd', 'derivative']
-travis_data_a=pd.read_csv("../test/lys/lys_a_travis.csv",sep='; ',header=0,engine='python')
-travis_data_a.columns = ['t', 'msd', 'derivative']
-vmd_data_c=pd.read_csv("../test/lys/vmd_c.dat",sep=' ',header=None)
-vmd_data_c.columns = ['t', 'msd']
-vmd_data_a=pd.read_csv("../test/lys/vmd_a.dat",sep=' ',header=None)
-vmd_data_a.columns = ['t', 'msd']
-vmd_data_a.t=vmd_data_a.t*1e3
-vmd_data_a.msd=vmd_data_a.msd*1e4
-vmd_data_c.t=vmd_data_c.t*1e3
-vmd_data_c.msd=vmd_data_c.msd*1e4
+
 
 x=np.array(x)
 y=np.array(y)
 z=np.array(z)
 q=np.array(q)
-print("LOADING TRAJECTORY FILE : COMPLETE")
-print("X : ",x.shape)
-print("Y : ",y.shape)
-print("Z : ",z.shape)
-print("q : ",q.shape)
-
-
 #select the indexes of anion and cations
 #is a tuple of array, one for axis
 cation_idx=np.where(q == 1)
 anion_idx=np.where(q == -1)
 
-#Deviations respect initial t
-dx=x[:,:]-x[0,:]
-dy=y[:,:]-y[0,:]
-dz=z[:,:]-z[0,:]
+#PRINTING INFO ON TRAJECTORY
+print("LOADING TRAJECTORY FILE : COMPLETE")
+if x.shape == y.shape == z.shape and q.shape[0] == x.shape[1] == y.shape[1] == z.shape[1]:
+	print("Total Number of Frame : ",x.shape[0])
+	print("Total length of trajectory : ",x.shape[0]*dt, "ps")
+	print("Total number of Molecules : ",x.shape[1])
+else:
+	raise Exception("SOMETHING WRONG WITH THE TRAJECTORY")
+#---------------------------
+#END OF TRAJECTORY LOADING
+#---------------------------
 
 
-#Product for selfdiffusion i*i
-r2=np.multiply(dx,dx)+np.multiply(dy,dy)+np.multiply(dz,dz)
 
-
-#UNIT CHANGES FROM A^2/ps -> pm^2/ps
-unit_conversion=1e4
-
-r2=r2*unit_conversion
-print("|R-R0|^2 : ",r2.shape)
-
-#SUBDATA FOR ANION AND CATION
-r2_cation=r2[:,cation_idx[0]]
-print("CATION SUB DATA : ",r2_cation.shape)
-r2_anion=r2[:,anion_idx[0]]
-print("ANION SUB DATA : ",r2_cation.shape)
-
-#T VECTOR
-t=np.arange(0,r2.shape[0]*dt,dt)
-print("T : ", t.shape )
-
-#SIMPLE MEAN OF MSD 
-msd_cation=np.sum(r2_cation,axis=1)/r2_cation.shape[1]
-print("MSD_CATION : ",msd_cation.shape)
-msd_anion=np.sum(r2_anion,axis=1)/r2_anion.shape[1]
-print("MSD_ANION : ",msd_anion.shape)
-
-
-def regression(msd,t,scaling=0.3):
-	idx=int(len(t)*scaling)
-	t_subset=t[idx:].reshape((-1,1))
-	msd_subset=msd[idx:]
-	print("SUB_T : " ,t_subset.shape)
-	print("SUB_MSD : ",msd_subset.shape)
-	model_c = LinearRegression().fit(t_subset,msd_subset)
-	print(f"slope CATION : {model_c.coef_} ")
-	print(f"Intercept: {model_c.intercept_} ")
-	print(f"D CATION : {model_c.coef_/6} pm^2/ps")
-	msd_pred = model_c.predict(t_subset)
-	return msd_pred, t_subset
-
-
-#REGRESSION
-#linear regression is done over 70% of the latter part of msd
-#CATION
-scaling=0.3
-idx=int(len(t)*scaling)
-t_subset=t[idx:].reshape((-1,1))
-msd_subset_c=msd_cation[idx:]
-print("SUB_T : " ,t_subset.shape)
-print("SUB_MSD : ",msd_subset_c.shape)
-model_c = LinearRegression().fit(t_subset,msd_subset_c)
-print(f"slope CATION : {model_c.coef_} ")
-print(f"Intercept: {model_c.intercept_} ")
-print(f"D CATION : {model_c.coef_/6} pm^2/ps")
-msd_pred_c = model_c.predict(t_subset)
-#ANION
-msd_subset_a=msd_anion[idx:]
-print("SUB_T : " ,t_subset.shape)
-print("SUB_MSD : ",msd_subset_a.shape)
-model_a = LinearRegression().fit(t_subset,msd_subset_a)
-print(f"slope ANION : {model_a.coef_}")
-print(f"Intercept: {model_a.intercept_} ")
-print(f"D ANION : {model_a.coef_/6} pm^2/ps")
-msd_pred_a = model_a.predict(t_subset)
-
-
-# #PLOTTING
-# fig, ax = plt.subplots(1,2)
-# ax[0].plot(t,msd_cation,linewidth=1.5,label=r'msd mean',color='red',zorder=2)
-# # for mol in r2_cation.T:
-# # 	 ax[0].plot(t,mol,linewidth=0.5,alpha=0.2,color='black',zorder=1)
-# ax[0].plot(t_subset,msd_pred_c,label='linear regression',linewidth=1,linestyle='dashed',color='orange',zorder=3)
-# ax[0].plot(travis_data.t,travis_data.msd,linewidth=1,linestyle='dotted',label=r'msd travis',color='blue',zorder=3)
-# ax[0].plot(vmd_data_c.t,vmd_data_c.msd,linewidth=1.4,linestyle='dotted',label=r'msd vmd',color='purple',zorder=3)
-# ax[0].legend()
-# ax[0].set_title("Cation MSD")
-# ax[0].set_xlabel(r'time / ps')
-# ax[0].set_ylabel(r'MSD / pm^2')
-# #ax[0].set_ylim([0,max(msd_cation)*3])
-# ax[0].set_box_aspect(1)
-
-
-# ax[1].plot(t,msd_anion,linewidth=1.5,label=r'msd mean',color='red',zorder=2)
-# # for mol in r2_anion.T:
-# # 	 ax[1].plot(t,mol,linewidth=0.5,alpha=0.2,color='black',zorder=1)
-# ax[1].plot(t_subset,msd_pred_a,label='linear regression',linewidth=1,linestyle='dashed',color='orange',zorder=3)
-# ax[1].plot(travis_data_a.t,travis_data_a.msd,linewidth=1,linestyle='dotted',label=r'msd travis',color='blue',zorder=3)
-# ax[1].plot(vmd_data_a.t,vmd_data_a.msd,linewidth=1,linestyle='dotted',label=r'msd vmd',color='purple',zorder=3)
-# ax[1].legend()
-# ax[1].set_title("Anion MSD")
-# #ax[1].set_ylim([0,max(msd_anion)*3])
-# ax[1].set_xlabel(r'time / ps')
-# ax[1].set_ylabel(r'MSD / pm^2')
-# ax[1].set_box_aspect(1)
-# fig.tight_layout()
-# plt.show()
 
 
 
 #-------------------------------------------------------------
 #CORRELATION DEPTH
-#correlation depth 1/2
-depth=0.95
+#is the percentage of the trajectory in which the correlation between ions is took in account
+depth=0.97
 
 print("-----------------------")
-print("TEST CORRELATION DEPTH")
+print("TEST CORRELATION DEPTH : ",depth*100," %")
 print("-----------------------")
 
-#ONLY CATION
+#CATION SUBDATA
 x_c=x[:,cation_idx[0]]
-print("cation X : ",x_c.shape)
 y_c=y[:,cation_idx[0]]
-print("cation Y : ",y_c.shape)
 z_c=z[:,cation_idx[0]]
-print("cation Z : ",z_c.shape)
-
 
 #loop over subsets
 subset_idx = np.arange(0,int(depth*x_c.shape[0]))
-print("interval index : ",len(subset_idx))
 max_origin_index = x_c.shape[0]-len(subset_idx)
-print("max_origin_index : ",max_origin_index)
+print("Number of intervals : ",max_origin_index)
 
-#FIRST
-print("ORIGIN : ",subset_idx[0])
-x=x_c[subset_idx,:]
-y=y_c[subset_idx,:]
-z=z_c[subset_idx,:]
-print("subset X : ",x.shape)
-print("subset Y : ",y.shape)
-print("subset Z : ",z.shape)
-
-#Deviations respect initial t
-dx=x[:,:]-x[0,:]
-dy=y[:,:]-y[0,:]
-dz=z[:,:]-z[0,:]
-
-#Product for selfdiffusion i*i
-r2=np.multiply(dx,dx)+np.multiply(dy,dy)+np.multiply(dz,dz)
-print("|R-R0|^2 : ",r2.shape)
-#mean over molecules
-r2=np.sum(r2,axis=1)/r2.shape[1]
-#UNIT CHANGES FROM A^2/ps -> pm^2/ps
-unit_conversion=1e4
-r2=r2*unit_conversion
-print("|R-R0|^2 MEAN : ",r2.shape)
-
-msd=r2
-i=1
-subset_idx=subset_idx+1
-
-#T VECTOR
-t_interval=np.arange(0,r2.shape[0]*dt,dt)
-print("T : ", t_interval.shape )
-
-#SECOND
+#
+msd = np.zeros(len(subset_idx))
+i=0
+#LOOP OVER INTERVALS
 while subset_idx[0]<max_origin_index :
+	if i%100 == 0:
+		print("Intervals processed : ",i)
 	x=x_c[subset_idx,:]
 	y=y_c[subset_idx,:]
 	z=z_c[subset_idx,:]
 
-	#Deviations respect initial t
+	#Deviations respect to the reference t0 of the interval
 	dx=x[:,:]-x[0,:]
 	dy=y[:,:]-y[0,:]
 	dz=z[:,:]-z[0,:]
 
 	#Product for selfdiffusion i*i
 	r2=np.multiply(dx,dx)+np.multiply(dy,dy)+np.multiply(dz,dz)
-	#mean over molecules
+	#mean over N molecules
 	r2=np.sum(r2,axis=1)/r2.shape[1]
 	#UNIT CHANGES FROM A^2/ps -> pm^2/ps
 	unit_conversion=1e4
@@ -279,24 +189,26 @@ while subset_idx[0]<max_origin_index :
 	msd=msd+r2
 	i+=1
 	subset_idx=subset_idx+1
+print("All Interval processed")
+#MSD MEAN OVER TOTAL NUMBER OF INTERVALS
+msd=msd/i
 
 #T VECTOR
 t_interval=np.arange(0,msd.shape[0]*dt,dt)
-print("i : ",i)
-print("MSD_FINAL : ",msd.shape)
-msd=msd/i
-msd_pred,t_subset = regression(msd,t_interval)
+msd_pred,t_pred = regression(msd,t_interval)
+plotting(t_interval,msd,t_pred,msd_pred)
 
 
-#PLOTTING
-fig, ax = plt.subplots(1,2)
-ax[0].plot(t_interval,msd,linewidth=1.5,label=r'msd mean',color='red',zorder=2)
-ax[0].plot(t_subset,msd_pred,label='linear regression',linewidth=1,linestyle='dashed',color='orange',zorder=3)
-ax[0].plot(travis_data.t,travis_data.msd,linewidth=1,linestyle='dotted',label=r'msd travis',color='blue',zorder=3)
-ax[0].plot(vmd_data_c.t,vmd_data_c.msd,linewidth=1.4,linestyle='dotted',label=r'msd vmd',color='purple',zorder=3)
-ax[0].legend()
-ax[0].set_title("Cation MSD")
-ax[0].set_xlabel(r'time / ps')
-ax[0].set_ylabel(r'MSD / pm^2')
-ax[0].set_box_aspect(1)
-plt.show()
+# #PLOTTING
+# fig, ax = plt.subplots(1,2)
+# ax[0].plot(t_interval,msd,linewidth=1.5,label=r'msd mean',color='red',zorder=2)
+# ax[0].plot(t_subset,msd_pred,label='linear regression',linewidth=1,linestyle='dashed',color='orange',zorder=3)
+# ax[0].plot(travis_data.t,travis_data.msd,linewidth=1,linestyle='dotted',label=r'msd travis',color='blue',zorder=3)
+# ax[0].plot(vmd_data_c.t,vmd_data_c.msd,linewidth=1.4,linestyle='dotted',label=r'msd vmd',color='purple',zorder=3)
+# ax[0].legend()
+# ax[0].set_title("Cation MSD")
+# ax[0].set_xlabel(r'time / ps')
+# ax[0].set_ylabel(r'MSD / pm^2')
+# ax[0].set_box_aspect(1)
+# plt.show()
+
